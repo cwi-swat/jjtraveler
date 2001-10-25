1f7a7fba17c2a3bb11edf6777b9b58132136bf36
Joost Visser <joost.visser@sig.nl>
2001-10-25 00:01:11 +0000

    * Added visitor combinator Backtrack(StateVisitor), which saves    	  the state of its argument visitor before executing it, and  	  restores this state afterwards. Note that the argument visitor  	  should clone its state before modifying it, otherwise state  	  restoration will not work properly.

--- src/jjtraveler/test/LibraryTest.java
+++ src/jjtraveler/test/LibraryTest.java
@@ -31,6 +31,24 @@ public class LibraryTest extends TestCase
 	    assertEquals("", n1.getLogger().getTrace());
 	}
     }
+
+    public void testBacktrack() 
+    throws jjtraveler.VisitFailure {
+	class Increment implements StateVisitor {
+	    public int state = 0;
+	    public Object getState() {return new Integer(state);}
+	    public void setState(Object o) {state = ((Integer) o).intValue();}
+	    public Visitable visit(Visitable x) {
+		state++; 
+		return x;
+	    }
+	}
+
+	Increment i = new Increment();
+	(new Backtrack(i)).visit(n1);
+	assertEquals(0,i.state);
+	assertEquals("",n1.getLogger().getTrace());
+    }	
 	
     public static Test suite() {
 	TestSuite suite = new TestSuite(jjtraveler.test.LibraryTest.class);
