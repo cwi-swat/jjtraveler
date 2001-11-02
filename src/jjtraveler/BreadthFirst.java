d16688255c8c2bac3b3fbf53d8c56b0c4e2a1e56
Joost Visser <joost.visser@sig.nl>
2001-11-02 00:49:07 +0000

    * Added second constructor method.

--- src/jjtraveler/BreadthFirst.java
+++ src/jjtraveler/BreadthFirst.java
@@ -1,11 +1,23 @@
 package jjtraveler;
 
+/**
+ * <code>BF(v) = Seq(v,All(EnQ),Try(Seq(DeQ,BF(v)))</code>
+ * <p>
+ * Visit a tree in breadth-first order.
+ */
+
 import java.util.LinkedList;
+import java.util.Collection;
 
 public class BreadthFirst {
 
     public BreadthFirst(Visitor v) {
 	pending = new LinkedList();
+	this.v = v;
+    }
+
+    public BreadthFirst(Visitor v, Collection c) {
+	pending = new LinkedList(c);
 	this.v = v;
     }
 
