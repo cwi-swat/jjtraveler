9383ea1049ed049ca19f5d03e76a9d14d1d20e6f
Joost Visser <joost.visser@sig.nl>
2001-11-14 10:00:30 +0000

    * Renamed TopDownUntil to TopDownWhile   * Added new TopDownUntil
          TopDownWhile(v) : descends while v succeeds
          TopDownUntil(v) : descends until v succeeds

--- src/jjtraveler/TopDownUntil.java
+++ src/jjtraveler/TopDownUntil.java
@@ -1,33 +1,25 @@
 package jjtraveler;
 
 /**
- * <code>TopDownUntil(v) = Choice(Sequence(v,All(TopDownUntil(v))),Identity)</code>
+ * <code>TopDownUntil(v) = Choice(v,All(TopDownUntil(v)))</code>
  * <p>
- * Visitor combinator with one visitor argument that applies this
- * visitor in pre-order fashion to all nodes, until it fails. Thus,
- * traversal is cut off below the nodes where failure occurs.
+ * Visitor combinator with one visitor argument that tries to apply
+ * this visitor in pre-order fashion to all nodes, until it succeeds.
+ * Thus, traversal is cut off below the nodes where success occurs.
+ * <p>
+ * In the Stratego Library, a strategy alltd(s) is present, that
+ * resembles this TopDownUntil(v) combinator.
  */
 
 public class TopDownUntil extends Choice {
 
     /* Create a visitor that applies its argument v in topdown
-     * fashion until it fails. Thus, traversal is cut off below
-     * the nodes where v fails.
+     * fashion until it succeeds. Thus, traversal is cut off below
+     * the nodes where v succeeds.
      */
     public TopDownUntil(Visitor v) {
-	super(null,
-	      new Identity());
-	first = new Sequence(v,new All(this));
-    }
-
-    /* Create a visitor that applies its argument v in topdown
-     * fashion until it fails, and subsequently applies its argument
-     * vFinally at the nodes where failure occurs.
-     */
-    public TopDownUntil(Visitor v, Visitor vFinally) {
-	super(null,
-	      vFinally);
-	first = new Sequence(v,new All(this));
+	super(v,null);
+	then = new All(this);
     }
 
 }
