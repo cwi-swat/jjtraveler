4da0ea9a93ef599fb1809d7e49468d54145e75f1
Arie van Deursen <arie.van.deursen@gmail.com>
2001-12-02 15:47:44 +0000

    Not FailAt

---
+++ src/jjtraveler/test/SucceedAtNodes.java
@@ -0,0 +1,19 @@
+package jjtraveler.test;
+
+import jjtraveler.*;
+
+/*
+ * Simple visitor recognizing two nodes given at creation time.
+ * Can be used to test generic visitors requiring a recognizing
+ * argument.
+ */
+
+public class SucceedAtNodes implements jjtraveler.Visitor {
+    Visitor success;
+    public SucceedAtNodes(Visitable n1, Visitable n2) {
+	success = new Not( new FailAtNodes(n1, n2) );
+    }
+    public Visitable visit(Visitable x) throws VisitFailure {
+	return success.visit(x);
+    }
+}
