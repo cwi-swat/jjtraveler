a7bb5b4377c421667d64063e6bf112c625ef264f
Arie van Deursen <arie.van.deursen@gmail.com>
2002-02-07 12:32:08 +0000

    Made DoWhileSuccess extend GuaranteeSuccess !
    __Arie

--- src/jjtraveler/DoWhileSuccess.java
+++ src/jjtraveler/DoWhileSuccess.java
@@ -1,11 +1,12 @@
 package jjtraveler;
 
-public class DoWhileSuccess extends TopDownWhile {
+public class DoWhileSuccess extends GuaranteeSuccess {
     
     public DoWhileSuccess ( Visitor condition, Visitor action ) {
-	super(new IfThenElse(condition,
-			     new Try(action),
-			     new Fail() ) );
+	super(new TopDownWhile(
+			       new IfThenElse(condition,
+					      new Try(action),
+					      new Fail() ) ) );
     }
 }
 
