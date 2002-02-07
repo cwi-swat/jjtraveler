c46e712bd6f1513c9526a106c99f611791aa020d
Arie van Deursen <arie.van.deursen@gmail.com>
2002-02-07 12:47:36 +0000

    Added javadocs.

--- src/jjtraveler/graph/WhileNotVisited.java
+++ src/jjtraveler/graph/WhileNotVisited.java
@@ -2,8 +2,16 @@ package jjtraveler.graph;
 
 import jjtraveler.*;
 
+/**
+ * Visit all nodes in the given graph exactly once,
+ * in a top down fashion.
+ */
+
 public class WhileNotVisited extends DoWhileSuccess {
 
+    /** 
+     * Carry out action for all nodes in the graph.
+     */
     public WhileNotVisited(Visitor action) {
 	super( new Not( new Visited() ), action );
     }
