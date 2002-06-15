066222c739d143522c9af912b5de20304542411a
Arie van Deursen <arie.van.deursen@gmail.com>
2002-06-15 13:53:13 +0000

    Actually added AllTest to the overal suite.

--- src/jjtraveler/TestAll.java
+++ src/jjtraveler/TestAll.java
@@ -1,7 +1,7 @@
 package jjtraveler;
 
 import junit.framework.*;
-
+ 
 public class TestAll extends TestCase {
 
     public TestAll(String test) {
@@ -10,6 +10,7 @@ public class TestAll extends TestCase {
 
     public static Test suite() {
 	TestSuite suite = new TestSuite();
+	suite.addTest(new TestSuite(AllTest.class));
 	suite.addTest(new TestSuite(jjtraveler.FailTest.class));
 	suite.addTest( new TestSuite(jjtraveler.IdentityTest.class) );
 	suite.addTest( new TestSuite(jjtraveler.IfThenElseTest.class) );
@@ -22,5 +23,8 @@ public class TestAll extends TestCase {
 	suite.addTest( new TestSuite(jjtraveler.CollectTest.class) );
 	return suite;
     }
-
+    
+    public static void main(String argv[]) {
+    	junit.textui.TestRunner.run(suite());
+    }
 }
