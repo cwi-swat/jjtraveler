28a9b1702d582f4f9a58e8c913046457a8869b1c
Arie van Deursen <arie.van.deursen@gmail.com>
2002-12-03 10:02:29 +0000

    Various test cases, explaining the behavior at different failure positions.

--- src/jjtraveler/SpineTopDownTest.java
+++ src/jjtraveler/SpineTopDownTest.java
@@ -8,36 +8,70 @@ public class SpineTopDownTest extends VisitorTestCase {
 	super(test);
     }
 
-    public void testSpineTopDownAtLeaves() {
-	Visitor failAtTwoLeaves = new FailAtNodes(n11, n12);
-	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(failAtTwoLeaves));
-	Logger expected = new Logger(failAtTwoLeaves, new Visitable[]{n0,n1,n11,n12,n2});
+    public void testSpineTopDownAtInnerNode() throws VisitFailure {
+	Visitor stop = new FailAtNodes(n1);
+	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(stop));
+
+	// n1 fails, so searching continues in n2.
+	Logger expected = new Logger(stop, new Visitable[]{n0,n1,n2});
+
+	Visitable nodeReturned = spineTopDown.visit(n0);
+
+	assertEquals("visit trace",expected, logger);
+	assertEquals("return value",n0, nodeReturned);
+    }
+
+
+
+    public void testSpineTopDownAtLeaf() throws VisitFailure {
+	Visitor stop = new FailAtNodes(n11);
+	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(stop));
+
+	// n11 fails, so path to n12 is first to succeed.
+	Logger expected = new Logger(stop, new Visitable[]{n0,n1,n11,n12});
+
+	Visitable nodeReturned = spineTopDown.visit(n0);
+
+	assertEquals("visit trace",expected, logger);
+	assertEquals("return value",n0, nodeReturned);
+    }
+
+    public void testSpineTopDownOnlySuccess() throws VisitFailure {
+	Visitor dontStop = new Identity();
+	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(dontStop));
+
+	// First path from n0 to n11 successful -- spinetopdown
+	// won't search any further after that.
+	Logger expected = new Logger(dontStop, new Visitable[]{n0,n1,n11});
+
+	Visitable nodeReturned = spineTopDown.visit(n0);
+
+	assertEquals("visit trace",expected, logger);
+	assertEquals("return value",n0, nodeReturned);
+    }
+
+
+
+
+    public void testSpineTopDownFailAtTop() {
+	Visitor stop = new FailAtNodes(n0);
+	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(stop));
+	Logger expected = new Logger(stop, new Visitable[]{n0});
+	Visitable nodeReturned = null;
 	try {
-	    Visitable nodeReturned = spineTopDown.visit(n0);
-	    assertEquals("visit trace",expected, logger);
-	    assertEquals("return value",n0, nodeReturned);
+	    nodeReturned = spineTopDown.visit(n0);
+	    fail("VisitFailure should have occured!");
 	} catch (VisitFailure vf) {
-	    fail("VisitFailure should not occur!");
+	    assertEquals("visit trace",expected, logger);
+	    assertNull("return value",nodeReturned);
 	}
     }
 
-    public void testSpineTopDownAtInnerNodes() {
-	Visitor failAtInners = new FailAtNodes(n1);
-	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(failAtInners));
-	Logger expected = new Logger(failAtInners, new Visitable[]{n0,n1,n2});
-	try {
-	    Visitable nodeReturned = spineTopDown.visit(n0);
-	    assertEquals("visit trace",expected, logger);
-	    assertEquals("return value",n0, nodeReturned);
-	} catch (VisitFailure vf) {
-	    fail("VisitFailure should not occur!");
-	}
-    }
 
-    public void testSpineTopDownAtFailure() {
-	Visitor isLeaf = new All(new Fail());
-	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(isLeaf));
-	Logger expected = new Logger(isLeaf, new Visitable[]{n0});
+    public void testSpineTopDownFailAtInners() {
+	Visitor stop = new FailAtNodes(n1,n2);
+	SpineTopDown spineTopDown = new SpineTopDown(logVisitor(stop));
+	Logger expected = new Logger(stop, new Visitable[]{n0,n1,n2});
 	Visitable nodeReturned = null;
 	try {
 	    nodeReturned = spineTopDown.visit(n0);
